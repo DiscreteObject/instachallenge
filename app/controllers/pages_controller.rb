57a47921b64c07abdb4d2aadf1342033e53782e1
Taylor Hudson <taylor.a.hudson@gmail.com>
2014-04-01 14:45:37 -0700

    add formatting to partial rendering

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,10 +10,10 @@ class PagesController < ApplicationController
     search_params = {
       :lat => params[:search_lat],
       :long => params[:search_long],
-      :dist => params[:search_distance]
+      :dist => params[:search_distance] * 1000
     }
 
-    @photos = get_photos(search_params)
+    @current_photos = get_photos(search_params)
 
   end
 end
--- spec/controllers/photo_controller_spec.rb
+++ spec/controllers/photo_controller_spec.rb
@@ -1,1 +1,20 @@
 require 'spec_helper'
+
+def get_photos(search_params)
+  query_string = 'https://api.instagram.com/v1/media/search?lat=' + search_params[:lat] + '&lng=' + search_params[:long] + '&distance=' + search_params[:dist] + '&client_id=' + ENV['INSTAGRAM_CLIENT_ID']
+
+  photo_results_data = JSON(HTTParty.get(query_string).body)['data']
+  current_photos = []
+  photo_results_data.each do |photo|
+    new_photo = Photo.new
+    new_photo.type = photo['type']
+    new_photo.link = photo['link']
+    new_photo.username = photo['user']['username']
+    new_photo.low_url= photo['images']['low_resolution']['url']
+    new_photo.thumb_url= photo['images']['thumbnail']['url']
+    new_photo.std_url= photo['images']['standard_resolution']['url']
+    # new_photo.save
+    current_photos << new_photo
+  end
+  return current_photos
+end
