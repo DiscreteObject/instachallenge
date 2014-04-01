8cc4b1c6f585fe2c50172bd6a8326ee9f5be6c8a
Taylor Hudson <taylor.a.hudson@gmail.com>
2014-04-01 07:13:34 -0700

    add uniqueness validation for link

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -4,4 +4,6 @@ describe Photo do
   it { should validate_presence_of(:type) }
   it { should validate_presence_of(:link) }
   it { should validate_presence_of(:username) }
+
+  it { should validate_uniqueness_of(:link) }
 end
