94f3840c2e0e7650d104ea54bc6329ea78413964
Taylor Hudson <taylor.a.hudson@gmail.com>
2014-04-01 06:28:54 -0700

    add info to home page

--- spec/features/visiting_home_page_spec.rb
+++ spec/features/visiting_home_page_spec.rb
@@ -4,7 +4,6 @@ feature "Visiting home page" do
   scenario "They see the home page" do
     visit '/'
     expect(page).to have_content "Welcome to Instachallenge"
-
+    expect(page).to have_content "Enter location info and radius"
   end
-
 end
