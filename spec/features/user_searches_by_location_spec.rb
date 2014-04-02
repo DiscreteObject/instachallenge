4a3d90ed8b4ed6aab14cc569ac0964dd7e7880ec
Taylor Hudson <taylor.a.hudson@gmail.com>
2014-04-01 20:16:37 -0700

    add image and link tags to photo display

--- spec/features/user_searches_by_location_spec.rb
+++ spec/features/user_searches_by_location_spec.rb
@@ -5,7 +5,7 @@ feature "User may search for media by lat/long coordinates" do
     visit '/'
     fill_in "latitude", with: "48.858844"
     fill_in "longitude", with: "2.294351"
-    fill_in "distance", with: "1000"
+    fill_in "distance", with: "1"
     click_on "Search"
 
     expect(page).to have_content("Here are your results:")
