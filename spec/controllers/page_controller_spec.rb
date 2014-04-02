e28becd4fbadc4dd4617e911690f5942ee97a062
Taylor Hudson <taylor.a.hudson@gmail.com>
2014-04-01 19:51:20 -0700

    fix texts broken because of model changes

--- spec/controllers/page_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-def main
-
-
-end
--- spec/features/user_searches_by_location_spec.rb
+++ spec/features/user_searches_by_location_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 feature "User may search for media by lat/long coordinates" do
   scenario do
     visit '/'
-    fill_in "search_lat", with: "48.858844"
-    fill_in "search_long", with: "2.294351"
-    fill_in "search_distance", with: "1000"
+    fill_in "latitude", with: "48.858844"
+    fill_in "longitude", with: "2.294351"
+    fill_in "distance", with: "1000"
     click_on "Search"
 
     expect(page).to have_content("Here are your results:")
